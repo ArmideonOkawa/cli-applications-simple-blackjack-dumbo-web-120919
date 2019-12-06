77b43aed38c39f35749795f2ed49e62c44cfdcb4
Jeremy Perry <jeremy.m.perry@gmail.com>
2019-12-06 07:46:34 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,58 +1,77 @@
 def welcome
- 
-  puts "Welcome to the Blackjack Table"
+  # code #welcome here
 end
 
 def deal_card
- 
-  return rand 1..11
+  rand(1..11)
 end
 
-def display_card_total (num)
-  
-  puts "Your cards add up to #{num}"
-  return num
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
+
+
+##########
+# rounds #
+##########
+
+def welcome
+  puts "Welcome to the Blackjack Table"
+end
+
 
 def prompt_user
   puts "Type 'h' to hit or 's' to stay"
 end
 
-def get_user_input
-  
-  gets.chomp
+
+def initial_round
+  first_round = deal_card + deal_card
+  display_card_total(first_round)
+  return first_round
 end
 
-def end_game (card_total)
-  
+def get_user_input
+  gets.chomp.strip
+end
+
+def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round
+
+
+def hit?(card_total)
+  valid_inputs = ["h", "s"]
+
+  prompt_user
+  user_input = get_user_input
+
+  until valid_inputs.include?(user_input)
+    invalid_command
+    prompt_user
+    user_input = get_user_input
+  end
+
+  if user_input == "h"
+    card_total += deal_card
+  end
+  card_total
   
-  display_card_total(deal_card+deal_card)
-  return deal_card + deal_card
+  ## Alternative solution - this solution calls hit? recursively if given an invalid command
+  # if user_input == "h"
+  #   card_total += deal_card
+  # elsif user_input == "s"
+  #   card_total
+  # else
+  #   invalid_command
+  #   hit?(card_total)
+  # end
+  
 end
 
 def invalid_command
- 
   puts "Please enter a valid command"
-  prompt_user
-  get_user_input
-end
-
-def hit? (number)
- 
-  prompt_user
-  answer = get_user_input
-  if answer == "s"
-    return number
-  elsif answer == "h"
-    new_card = deal_card
-    return number + new_card
-  else
-    return invalid_command
-  end
 end
 
 
